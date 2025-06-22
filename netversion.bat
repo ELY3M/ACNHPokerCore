754952f36ea3649d1d43035a4d4d364f592bc421
ELY M. <elymbmx@gmail.com>
2025-06-21 20:50:30 -0500

    test22

--- netversion.bat
+++ netversion.bat
@@ -1,4 +1,4 @@
 @echo off
 FOR /F "delims=" %%i IN ('dotnet msbuild ACNHPokerCore/ACNHPokerCore.csproj --getProperty:TargetFramework') DO (SET netversion=%%i)
 echo netversion is: %netversion%
-pause
+echo %netversion%>>netversion.txt
